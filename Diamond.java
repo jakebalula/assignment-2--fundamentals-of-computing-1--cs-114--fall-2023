83fbf18d4c573b3fe2418522986928e55120f43a
jakebalula <balula@hartford.edu>
2023-10-09 13:38:37 -0400

    Even or odd input detection

--- Diamond.java
+++ Diamond.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
 public class Diamond {
   public static void main(String[] args) {
+    int number;
+    System.out.println("Please enter a number");
+    Scanner scan = new Scanner(System.in);
+    number = scan.nextInt();
+    if (number % 2 == 0)
+      System.out.println("Even");
+    else
+      System.out.println("odd");
+
 
   }
 }
