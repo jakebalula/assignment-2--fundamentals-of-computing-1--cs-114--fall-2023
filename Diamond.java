42f28bf9e475f06a006ab44e0b5f1b532a37c200
jakebalula <balula@hartford.edu>
2023-10-11 16:19:04 -0400

    Update Diamond.java

--- Diamond.java
+++ Diamond.java
@@ -1,32 +1,15 @@
 import java.util.Scanner;
 public class Diamond {
   public static void main(String[] args) {
-    int row;
     int i;
     int j;
+    int spaces;
     System.out.println("Please enter a number");
     Scanner scan = new Scanner(System.in);
-    row = scan.nextInt();
-    if (row % 2 == 1) {//Sees if input is even or odd
-      for (i = 0 ; i < row; i++) {
-        for (j = 0; j < row - i - 1; j++) {
-          System.out.print(" ");
-        }
-        for (j = 0; j <= i; j++){
-          System.out.print("*");
-        }
-        System.out.println();
-      }
-      for (i = 0; i < row-1; i++) {
-        for (j=0;j<=i; j++) {
-          System.out.print(" ");
-      }
-      for (j = 0; j<row-i-1; j++) {
+    i = scan.nextInt();
+    if (i % 2 == 1) {//Sees if input is even or odd
+      for (j = 0; j <= i-1; j++)
         System.out.print("*");
-      }
-      System.out.println();
-    }
-    //else
     }
   }
 }
