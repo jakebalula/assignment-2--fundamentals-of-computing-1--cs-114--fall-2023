aaf71b29dd98a3a78d62650f369b8097b739180e
jakebalula <balula@hartford.edu>
2023-10-11 23:02:07 -0400

    still stuck

--- Diamond.java
+++ Diamond.java
@@ -2,22 +2,24 @@ import java.util.Scanner;
 public class Diamond {
   public static void main(String[] args) {
     int number;
-    int j;
-    int i;
+    int row;
+    int col;
     System.out.println("Please enter a number");
     Scanner scan = new Scanner(System.in);
     number = scan.nextInt();
     if (number % 2 == 1) {//Sees if input is even or odd
-
-      for (i = 0; i < number; i++) {
-        for (j = 0; j < number-i; j++) {
-          System.out.print(" ");
-        }
-        for (j = 0; j <= i; j++) {
+      for (row = 1; row <= number; row++) {
+        for (col = 0; col < row; col++) {
           System.out.print("*");
-        }
-        System.out.println();
+       }
+       System.out.println();
+      }
+      for (row = number - 1; row > 0; row--) {
+                for (col = 0; col < row; col++) {
+                  System.out.print("*");
+          }
+          System.out.println();
+      }
       }
     }
   }
-}
