dfd785815a699c513b1bd4e982dc6655b20db0cf
jakebalula <balula@hartford.edu>
2023-10-12 16:25:09 -0400

    Finished Odd diamond

--- Diamond.java
+++ Diamond.java
@@ -10,20 +10,20 @@ public class Diamond {
     number = scan.nextInt();
     if (number % 2 == 1) {//Sees if input is even or odd
       //Odd Diamond
-      for (row = 0; row <= number; row++) { //Top half
-        for (col = row - 1; col <= number; col++) {//print spaces
+      for (row = 1; row <= (number + 1) / 2; row++) { //Top half
+        for (col = 1; col <= (number + 1) / 2 - row; col++) {//print spaces
           System.out.print(" ");
        }
-        for (star = 1; star <= row; star++) { //print stars
+        for (star = 1; star <= 2 * row - 1; star++) { //print stars
           System.out.print("*");
         }
         System.out.println();
       }
-      for (row = number - 1; row >= 1; row--) { //Bottom half
-        for (col = number; col >= row; col--) { //Prints spaces
+      for (row = (number - 1) / 2; row >= 1; row--) { //Bottom half
+        for (col = 1; col <= (number + 1) / 2 - row; col++) { //Prints spaces
           System.out.print(" ");
         }
-        for (star = 1; star <= row ; star++) { //Prints Stars
+        for (star = 1; star <= 2 * row - 1; star++) { //Prints Stars
           System.out.print("*");
           }
         System.out.println();
