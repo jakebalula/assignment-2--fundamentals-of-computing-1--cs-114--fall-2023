f1fb1215dc7bdfed77fe19f3e72b58816f7a240e
jakebalula <balula@hartford.edu>
2023-10-12 16:06:26 -0400

    Updates to odd diamond

--- Diamond.java
+++ Diamond.java
@@ -1,25 +1,33 @@
 import java.util.Scanner;
 public class Diamond {
   public static void main(String[] args) {
-    int number;
+    int number = 0;
     int row;
     int col;
+    int star;
     System.out.println("Please enter a number");
     Scanner scan = new Scanner(System.in);
     number = scan.nextInt();
     if (number % 2 == 1) {//Sees if input is even or odd
-      for (row = 1; row <= number; row++) {
-        for (col = 0; col < row; col++) {
-          System.out.print("*");
+      //Odd Diamond
+      for (row = 0; row <= number; row++) { //Top half
+        for (col = row - 1; col <= number; col++) {//print spaces
+          System.out.print(" ");
        }
-       System.out.println();
+        for (star = 1; star <= row; star++) { //print stars
+          System.out.print("*");
+        }
+        System.out.println();
       }
-      for (row = number - 1; row > 0; row--) {
-                for (col = 0; col < row; col++) {
-                  System.out.print("*");
+      for (row = number - 1; row >= 1; row--) { //Bottom half
+        for (col = number; col >= row; col--) { //Prints spaces
+          System.out.print(" ");
+        }
+        for (star = 1; star <= row ; star++) { //Prints Stars
+          System.out.print("*");
           }
-          System.out.println();
+        System.out.println();
       }
-      }
+     }
     }
   }
